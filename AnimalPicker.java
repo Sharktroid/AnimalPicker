81170c957caeb8d39a2c3a3665551d446dc786cd
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-02-25 16:47:16 -0500

    Added in fish option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -8,14 +8,24 @@ public class AnimalPicker {
 			System.out.println("What animal would you like to see?");
 			String selection = scanner.nextLine();
 			if (selection.toLowerCase().equals("cat")) {
-				System.out.println("  |\\      _,,,---,,_\r\n" + //
-										"  /,`.-'`'    -.  ;-;;,_\r\n" + //
-										" |,4-  ) )-,_. ,\\ (  `'-'\r\n" + //
-										"'---''(_/--'  `-'\\_)");
+				System.out.println(	"  |\\      _,,,---,,_\r\n" + //
+									"  /,`.-'`'    -.  ;-;;,_\r\n" + //
+									" |,4-  ) )-,_. ,\\ (  `'-'\r\n" + //
+									"'---''(_/--'  `-'\\_)");
 				repeat = false;
 			}
 			else if (selection.toLowerCase().equals("dog")) {
-				System.out.println("__\r\n (___()'`;\r\n /,    /`\r\n \\\\\"--\\\\");
+				System.out.println(	"__\r\n" + //
+									" (___()'`;\r\n" + //
+									" /,    /`\r\n" + //
+									" \\\\\"--\\\\");
+				repeat = false;
+			}
+			else if (selection.toLowerCase().equals("fish")) {
+				System.out.println(	"      /\"*._         _\r\n" + //
+									"  .-*'`    `*-.._.-'/\r\n" + //
+									"< * ))     ,       ( \r\n" + //
+									"  `*-._`._(__.--*\"`.\\");
 				repeat = false;
 			}
 			else {
